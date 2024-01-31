1a37c819b912739ebf348520cd7a92bddcbd297e
Yosuke Iwata <s20970059@nucba.ac.jp>
2024-01-31 22:22:24 +0900

    初期コミット

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Button.css';
+import { Link } from 'react-router-dom';
+
+const Button = (children, type, onClick, buttonStyle, buttonSize) => {
+  return (
+    <div>Button</div>
+  )
+}
+
+export default Button
